403005c7e6708bd0bdca898947a856d97e1f2339
Antonio CIrillo <dev.cirillo@gmail.com>
2022-01-12 22:56:40 +0100

    init nft contract

---
+++ contracts/Drawing.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.11;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract Drawing is ERC721 {
+
+    constructor() ERC721("Drawing", "DRAW") public { }
+
+}
