36549797c7bd010910ffea97eef879bddbeda781
Antonio CIrillo <dev.cirillo@gmail.com>
2022-01-12 22:56:31 +0100

    inital commit

---
+++ truffle-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,     
+      network_id: "*",       // Any network (default: none)
+    }
+  },
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+      version: "0.8.11",    // Fetch exact version from solc-bin (default: truffle's version)
+    }
+  }
+
+};
