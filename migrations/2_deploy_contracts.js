ee5c620b9e9b9ff58172479e39f2d88f4f930db7
Antonio CIrillo <79747869+antonio-cirillo@users.noreply.github.com>
2022-01-22 01:35:08 +0100

    update deploy function

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,9 @@
 const Drawing = artifacts.require('Drawing');
+const DrawingMarket = artifacts.require('DrawingMarket');
 
 module.exports = function(deployer) {
-    deployer.deploy(Drawing);
+    deployer.deploy(DrawingMarket)
+    .then(() => {
+        return deployer.deploy(Drawing, DrawingMarket.address);
+    });
 }
