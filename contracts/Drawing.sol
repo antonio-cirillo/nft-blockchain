bc0b5624fb5e03790455a7f9430dd002e0f2425b
Antonio CIrillo <79747869+antonio-cirillo@users.noreply.github.com>
2022-01-22 01:36:04 +0100

    update Drawing contract

--- contracts/Drawing.sol
+++ contracts/Drawing.sol
@@ -2,47 +2,59 @@
 pragma solidity 0.8.11;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "base64-sol/base64.sol";
 
 contract Drawing is ERC721URIStorage {
 
-    uint256 private _tokenCounter;
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
 
-    constructor() ERC721("Drawing", "DRAW") public {
-        _tokenCounter = 0; 
+    address contractAddress;
+
+    constructor(address marketplaceAddress) ERC721("Drawing", "DRAW") public { 
+        contractAddress = marketplaceAddress;
     }
 
-    function create(string memory _name, string memory _description, string memory _svg) public {
-        _safeMint(msg.sender, _tokenCounter);
-        string memory _imageURI = svgToImageURI(_svg);
-        string memory _tokenURI = createTokenURI(_name, _description, _imageURI);
-        _setTokenURI(_tokenCounter, _tokenURI);
-        _tokenCounter = _tokenCounter + 1;
+    function createToken(string memory name, string memory description, 
+            string memory svg) public returns (uint) {
+
+        _tokenIds.increment();
+        uint256 tokenCounter = _tokenIds.current();
+
+        _safeMint(msg.sender, tokenCounter);
+        string memory imageURI = svgToImageURI(svg);
+        string memory tokenURI = createTokenURI(name, description, imageURI);
+        _setTokenURI(tokenCounter, tokenURI);
+        setApprovalForAll(contractAddress, true);
+        
+        return tokenCounter;
+    
     }
 
     // Format: data:image/svg+xml;base64,(Base64-encoding)
-    function svgToImageURI(string memory _svg) public pure returns (string memory) {
+    function svgToImageURI(string memory svg) public pure returns (string memory) {
 
-        string memory _base = "data:image/svg+xml;base64,";
-        string memory _base64encoded = Base64.encode(
-            bytes(string(abi.encodePacked(_svg))));
-        string memory imageURI = string(abi.encodePacked(_base, _base64encoded));
+        string memory base = "data:image/svg+xml;base64,";
+        string memory base64encoded = Base64.encode(
+            bytes(string(abi.encodePacked(svg))));
+        string memory imageURI = string(abi.encodePacked(base, base64encoded));
         return imageURI;
     
     }
 
-    function createTokenURI(string memory _name, string memory _description, 
-            string memory _imageURI) public pure returns (string memory) {
+    function createTokenURI(string memory name, string memory description, 
+            string memory imageURI) public pure returns (string memory) {
         
-        string memory _base = "data:application/json;base64,";
+        string memory base = "data:application/json;base64,";
         return string(abi.encodePacked(
-            _base,
+            base,
             Base64.encode(
                 bytes(abi.encodePacked(
-                    '{"name": "', _name, '"', 
-                    ', "description": "', _description, '"', 
+                    '{"name": "', name, '"', 
+                    ', "description": "', description, '"', 
                     ', "attributes": ""',
-                    ', "image": "', _imageURI, '" }'))
+                    ', "image": "', imageURI, '" }'))
             )
         ));
    
