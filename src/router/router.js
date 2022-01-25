c58cf6e02c100bbbe6cbc384fbb27c001e9dc337
Antonio CIrillo <dev.cirillo@gmail.com>
2022-01-25 21:28:43 +0100

    added route on /

--- src/router/router.js
+++ src/router/router.js
@@ -3,6 +3,10 @@ const path = require('path');
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    res.render('index');
+})
+
 router.get('/create-nft', (req, res) => {
     res.render('create-nft');
 })
@@ -10,6 +14,7 @@ router.get('/create-nft', (req, res) => {
 router.get('/my-nft', (req, res) => {
     res.render('my-nft', {
         created: (req.query.action && req.query.action == 'created') ? true : false,
+        selling: (req.query.action && req.query.action == 'selling') ? true : false,
         error: (req.query.action && req.query.action == 'error') ? true : false
     })
 })
