72cae9bafcfd57a544042d090c63b2fbbcbe917b
Antonio CIrillo <dev.cirillo@gmail.com>
2022-01-25 02:17:53 +0100

    handled query paramters

--- src/router/router.js
+++ src/router/router.js
@@ -7,4 +7,11 @@ router.get('/create-nft', (req, res) => {
     res.render('create-nft');
 })
 
+router.get('/my-nft', (req, res) => {
+    res.render('my-nft', {
+        created: (req.query.action && req.query.action == 'created') ? true : false,
+        error: (req.query.action && req.query.action == 'error') ? true : false
+    })
+})
+
 module.exports = router;
